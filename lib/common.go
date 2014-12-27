d2bf9d1caf895120cf08c3e8fbad156ada1e50a5
Eric Dennis <eric.dennis@sleepygiant.com>
2014-12-27 05:33:12 -0800

    Add common.Init() for proper redis connection

--- lib/common.go
+++ lib/common.go
@@ -1,14 +1,17 @@
 package common
 
 import (
-  "code.google.com/p/gorilla/mux"
   "encoding/json"
   "errors"
   "net/http"
   "net/url"
   "time"
+  "strconv"
+
+  "github.com/kelseyhightower/envconfig"
   godis "github.com/simonz05/godis/redis"
-  "log"
+  "github.com/gorilla/mux"
+  "github.com/golang/glog"
 )
 
 const (
@@ -17,10 +20,11 @@ const (
   HTTP    = "http"
 )
 
-var (
-  redis        *godis.Client
-  filenotfound string
-)
+type RedisConf struct {
+  Host string
+  DB   string
+  Pass string
+}
 
 type Redirect struct {
   Key          string
@@ -29,6 +33,12 @@ type Redirect struct {
   CreationDate int64
   Clicks       int64
 }
+
+var (
+  redis        *godis.Client
+  filenotfound string
+  redisconf    RedisConf
+)
 
 // Converts the Redirect to JSON.
 func (r Redirect) Json() []byte {
@@ -62,6 +72,7 @@ func store(key, sourceurl, targeturl string) *Redirect {
 // loads the Redirect for the given key. If the key is
 // not found, os.Error is returned.
 func load(key string) (*Redirect, error) {
+  glog.Infof("Loading redis key: %s", key)
   if ok, _ := redis.Hexists(key, "SourceUrl"); ok {
     redir := new(Redirect)
     redir.Key = key
@@ -104,22 +115,55 @@ func load(key string) (*Redirect, error) {
 
 // function to translate a URL and redirect
 func Resolve(w http.ResponseWriter, r *http.Request) {
+  glog.Infof("request: %+v", r)
+  glog.Infof("request mux vars: %+v", mux.Vars(r))
+
   scheme := mux.Vars(r)["scheme"]
   host := mux.Vars(r)["host"]
   path := mux.Vars(r)["path"]
   u, err := url.Parse("")
   if err != nil {
-    log.Fatal(err)
+    glog.Fatal(err)
   }
   u.Scheme = scheme
   u.Host = host
   u.Path = path
 
+  if u.Scheme == "" {
+    u.Scheme = "http"
+  }
+
+  glog.Infof("url: %s", u.String())
+
   redir, err := load(u.String())
   if err == nil {
+    glog.Infof("Found source URL %s, redirecting to target URL %s", redir.Key, redir.TargetUrl)
     go redis.Hincrby(redir.Key, "Clicks", 1)
     http.Redirect(w, r, redir.TargetUrl, http.StatusMovedPermanently)
   } else {
+    glog.Infof("Error: %s. Redirecting to default target URL %s", err, filenotfound)
     http.Redirect(w, r, filenotfound, http.StatusMovedPermanently)
   }
+}
+
+func Init() {
+  err := envconfig.Process("redis", &redisconf)
+
+  if redisconf.Host == "" {
+    redisconf.Host = "tcp:localhost:6379"
+  }
+  if redisconf.DB == "" {
+    redisconf.DB = "0"
+  }
+
+  host := redisconf.Host
+  db, err := strconv.Atoi(redisconf.DB)
+  if err != nil {
+    glog.Fatal(err)
+  }
+  passwd := redisconf.Pass
+
+  redis = godis.New(host, db, passwd)
+
+  filenotfound = "http://www.google.com"
 }
